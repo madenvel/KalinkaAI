0a96dfba5ab58ab2ebbc89639b273b4cc5fc8a7b
Dmitry Savin <envelsavinds@gmail.com>
2026-03-07 23:37:12 +0000

    Update connection status padding

--- lib/widgets/header_zone.dart
+++ lib/widgets/header_zone.dart
@@ -257,7 +257,7 @@ class _HeaderZoneState extends ConsumerState<HeaderZone>
             : null,
         behavior: HitTestBehavior.opaque,
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 17, horizontal: 8),
+          padding: const EdgeInsets.symmetric(vertical: 17, horizontal: 0),
           child: dot,
         ),
       ),
